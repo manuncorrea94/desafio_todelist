73607b91d1333016cdcbd22612ba76e847f7e19f
Emanuele Correa <manuncorra@hotmail.com>
2019-09-09 16:29:54 -0300

    testando os arquviso js

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,78 +1,71 @@
 <template>
+  <div class="">
+    <!--Menu-->
+    <div class="menu">
+      <ul class="nav justify-content-end container">
+        <li class="nav-item">
+          <a class="nav-link active" href="index.html">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Formulario</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#gerardias">Gerar dias</a>
+        </li>
+      </ul>
+    </div><!--Fim menu-->
 
-<div class="">
-<!--Menu-->
-
-<div class="menu">
-  <ul class="nav justify-content-end container">
-  <li class="nav-item">
-    <a class="nav-link active" href="#">Home</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Formulario</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Relatorio</a>
-  </li>
-  </ul>
-</div><!--Fim do menu-->
-
-<div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-  <h1 class="display-4">To Do List</h1>
-  <p class="lead">Os códigos da tarefa devera ser disponibilizado no GitHub</p>
-</div>
-
-<!--Formulario-->
-<form>
-  <div class="container">
-  <div class="form-row">
-
-    <div class="form-group col-md-4">
-      <label for="inoutText">Titulo do Produto:</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Titulo">
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h1 class="display-4">To Do List</h1>
+      <p class="lead">Os códigos da tarefa devera ser disponibilizado no GitHub</p>
     </div>
+    <!--Formulario-->
+    <form>
+      <div class="container">
+        <div class="form-row">
 
-    <div class="form-group col-md-8">
-      <label for="inoutDescription">Descrição:</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Descrição">
+          <div class="form-group col-md-4">
+            <label for="inoutText">Titulo do Produto:</label>
+            <input type="email" class="form-control" id="titulo" placeholder="Titulo">
+        </div>
+        <div class="form-group col-md-8">
+          <label for="inoutDescription">Descrição:</label>
+          <input type="email" class="form-control" id="descricao" placeholder="Descrição">
+        </div>
+        <div class="form-group col-md-6">
+          <label for="inoutPontuacao">Pontuação Numerica:</label>
+          <select class="form-control" id="exampleFormControlSelect1">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+            <option>9</option>
+            <option>10</option>
+            <option>11</option>
+            <option>12</option>
+            <option>13</option>
+          </select>
+        </div>
+
+        <div class="form-group col-md-6">
+          <label for="inoutDia">Dia:</label>
+          <input type="text" class="form-control" id="calendario" >
+        </div>
+
+        </div>
+        <button id="btn" type="button" class="btn btn-success">Adicionar</button>
+
+      </div>
+    </form><!--fim formulario-->
+
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h6 class="display-4">Gerar Dias:</h6>
     </div>
-
-    <div class="form-group col-md-4">
-      <label for="inoutPontuacao">Pontuação Numerica:</label>
-        <select class="form-control" id="exampleFormControlSelect1">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-          <option>6</option>
-          <option>7</option>
-          <option>8</option>
-          <option>9</option>
-          <option>10</option>
-          <option>11</option>
-          <option>12</option>
-          <option>13</option>
-      </select>
-    </div>
-
-    <div class="form-group col-md-4">
-      <label for="inoutDia">Dia:</label>
-      <input type="text" class="form-control" id="calendario" >
-    </div>
-
   </div>
-  <button type="button" class="btn btn-success">Adicionar</button>
-  <button type="button" class="btn btn-primary">Gerar Dias</button>
-  </div>
-</form><!--fim formulario-->
-
-<div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-  <h6 class="display-4">Gerar Relatorio</h6>
-</div>
-</div>
-
-
 </template>
 
 <script>
@@ -80,14 +73,11 @@ export default {
   name: 'Home',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+
     }
   }
-}
 
-$(function() {
-	    $( "#calendario" ).datepicker();
-	});
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -106,15 +96,36 @@ $(function() {
 h1, h2 {
   font-weight: normal;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
+
+table{
+    padding: 0px;
+    color: white;
+    background-color: #D699AD;
+    border-style: solid;
+    border-width: 2px;
+    border-color: #8A2E2E;
+
+}
+#borda{
+    display: none;
+    color:green;
+    border-style: solid;
+    border-width: 3px;
+    border-color: #7A0000;
+}
+
 </style>
---
+++ static/js/teste.js
@@ -0,0 +1,20 @@
+$(function() {
+  $( "#calendario" ).datepicker();
+});
+
+let lista =[]
+
+lista[0].titulo="titulo";
+lista[0].descricao="descricao";
+
+
+lista[0].pontuacao=5;
+lista[1].pontuacao=1;
+lista[2].pontuacao=10
+lista[3].pontuacao=10;
+
+$(document).ready(function(){
+  $("#btn").click(function(){
+  f();
+  });
+});
